3a4c14805003c633f27a1cd2a77018b09c672198
rautelaprakash543@gmail.com <56201964+OptiMus-cyber@users.noreply.github.com>
2022-08-23 01:34:32 +0530

    fix port binding

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require('./database');
 const session = require("express-session");
 
 
-const server = app.listen(port,()=>console.log("Server listening on port: "+port));
+const server = app.listen(process.env.PORT || port,()=>console.log("Server listening on port: "+port));
 
 app.set("view engine","pug");
 app.set("views","views");
